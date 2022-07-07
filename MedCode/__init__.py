c731d9d020d25330b903563704ff4c819cfbe82f
ycq091044 <ycqsjtu@gmail.com>
2022-07-07 16:47:26 -0500

    get fully connected code mappings with all paths on the graph, not the shortest path - add cache logics to google drive files to prevent 403 error

--- MedCode/__init__.py
+++ MedCode/__init__.py
@@ -4,8 +4,9 @@ import pandas as pd
 import time
 from urllib import request
 import sys
+import os
 
-__version__ = "1.1"
+__version__ = "1.2"
 
 # README
 README = """
@@ -48,22 +49,21 @@ class CodeMapping:
         self.Codes = list(Codes)
         # codes Graph
         self.G = nx.Graph()
+
+        if not os.path.exists('./data'):
+            os.makedirs('./data')
         
     def load(self):
         tic = time.time()
         print ("----- preparing code mappings -----")
         # NDC, RXCUI, ATC4
         self.load_RXCUI2atc4()
-        print ('source 1 loaded from https://drive.google.com/uc?id=1I2G6fsBDXDiAK95qFWwtnl3Ib2MaLeCx')
         # RxNorm, RXCUI
         self.load_rxnorm2RXCUI()
-        print ('source 2 loaded from https://drive.google.com/uc?id=1d2HzsByXrPadvjaKDOEaOt78OkAZOrjC')
         # Name, SMILES
         self.load_drugbank()
-        print ('source 3 loaded from https://drive.google.com/uc?id=199i8mP2gMQNhwUe-2ZNmIr5fhiBbzVlK')
         # RxNorm, Name
         self.load_RxNorm2Name()
-        print ('source 4 loaded from https://drive.google.com/uc?id=1Z11J4st1sI44jPborls9jIxzcpF-GpGt')
         # create fully connected graph of Codes (as nodes)
         self.fully_connected_codes()
         print ('load time: {}s'.format(time.time() - tic))
@@ -71,25 +71,51 @@ class CodeMapping:
         
         
     def load_drugbank(self):
-        name2SMILES = pd.read_csv(request.urlopen('https://drive.google.com/uc?id=199i8mP2gMQNhwUe-2ZNmIr5fhiBbzVlK'))
+        if not os.path.exists('./data/name2SMILES.csv'):
+            name2SMILES = pd.read_csv(request.urlopen('https://drive.google.com/uc?id=199i8mP2gMQNhwUe-2ZNmIr5fhiBbzVlK'))
+            name2SMILES.to_csv('./data/name2SMILES.csv', index=False)
+            print ('source loaded from https://drive.google.com/uc?id=199i8mP2gMQNhwUe-2ZNmIr5fhiBbzVlK')
+        else:
+            name2SMILES = pd.read_csv('./data/name2SMILES.csv')
+            print ('source loaded from ./data cache')
         self.Name_to_SMILES = self.mapping_from_pd(name2SMILES, 'name', 'moldb_smiles')
         self.G.add_edge('Name', 'SMILES')
     
     def load_RxNorm2Name(self):
-        RxNorm2Name = pd.read_csv(request.urlopen('https://drive.google.com/uc?id=1Z11J4st1sI44jPborls9jIxzcpF-GpGt'))
+        if not os.path.exists('./data/RxNorm2Name.csv'):
+            RxNorm2Name = pd.read_csv(request.urlopen('https://drive.google.com/uc?id=1Z11J4st1sI44jPborls9jIxzcpF-GpGt'))
+            RxNorm2Name.to_csv('./data/RxNorm2Name.csv', index=False)
+            print ('source loaded from https://drive.google.com/uc?id=1Z11J4st1sI44jPborls9jIxzcpF-GpGt')
+        else:
+            RxNorm2Name = pd.read_csv('./data/RxNorm2Name.csv')
+            print ('source loaded from ./data cache')
         self.RxNorm_to_Name = self.mapping_from_pd(RxNorm2Name, 'RxNorm', 'Name')
         self.G.add_edge('RxNorm', 'Name')
         
     def load_RXCUI2atc4(self):
-        RXCUI2atc4 = pd.read_csv(request.urlopen('https://drive.google.com/uc?id=1I2G6fsBDXDiAK95qFWwtnl3Ib2MaLeCx'))
+        if not os.path.exists('./data/RXCUI2atc4.csv'):
+            RXCUI2atc4 = pd.read_csv(request.urlopen('https://drive.google.com/uc?id=1I2G6fsBDXDiAK95qFWwtnl3Ib2MaLeCx'))
+            RXCUI2atc4.to_csv('./data/RXCUI2atc4.csv', index=False)
+            print ('source loaded from https://drive.google.com/uc?id=1I2G6fsBDXDiAK95qFWwtnl3Ib2MaLeCx')
+        else:
+            RXCUI2atc4 = pd.read_csv('./data/RXCUI2atc4.csv')
+            print ('source loaded from ./data cache')
         Codes = ['NDC', 'RXCUI', 'ATC4']
         for code1, code2 in combinations(Codes, 2):
             exec("self.{0}_to_{1} = self.mapping_from_pd(RXCUI2atc4, '{0}', '{1}')".format(code1, code2))
             self.G.add_edge(code1, code2)
-    
+
     def load_rxnorm2RXCUI(self):
-        respond = request.urlopen('https://drive.google.com/uc?id=1d2HzsByXrPadvjaKDOEaOt78OkAZOrjC')
-        self.RxNorm2RXCUI = eval(respond.read())
+        if not os.path.exists('./data/rxnorm2RXCUI.txt'):
+            respond = request.urlopen('https://drive.google.com/uc?id=1d2HzsByXrPadvjaKDOEaOt78OkAZOrjC')
+            self.RxNorm2RXCUI = eval(respond.read())
+            with open('./data/rxnorm2RXCUI.txt', 'w') as outfile:
+                print (self.RxNorm2RXCUI, file=outfile)
+            print ('source loaded from https://drive.google.com/uc?id=1d2HzsByXrPadvjaKDOEaOt78OkAZOrjC')
+        else:
+            infile = open('./data/rxnorm2RXCUI.txt', 'r')
+            self.RxNorm2RXCUI = eval(infile.read())
+            print ('source loaded from ./data cache')
         self.RxNorm_to_RXCUI = {k: [v] for k, v in self.RxNorm2RXCUI.items()}
         self.G.add_edge('RxNorm', 'RXCUI')
         
@@ -119,6 +145,7 @@ class CodeMapping:
     def fully_connected_codes(self):
         for code1, code2 in combinations(self.Codes, 2):
             self.add_mapping(code1, code2)
+            self.add_mapping(code2, code1)
 
     def add_mapping(self, code1, code2):
         if code1 != 'SMILES':
@@ -126,28 +153,14 @@ class CodeMapping:
                 if hasattr(self, "{}_to_{}".format(code2, code1)):
                     exec("self.{0}_to_{1} = self.mapping_reverse_dict(self.{1}_to_{0})".format(code1, code2))
                 else:
-                    shortest_path = nx.shortest_path(self.G, source=code1, target=code2)
-                    key = shortest_path[0]
-                    for i in range(len(shortest_path) - 2):
-                        mid, value = shortest_path[i+1: i+3]
-                        self.map_combine(key, mid, value)
-                        self.G.add_edge(key, value)
+                    for path in nx.all_simple_paths(self.G, source=code1, target=code2):
+                        key = path[0]
+                        for i in range(len(path) - 2):
+                            mid, value = path[i+1: i+3]
+                            self.map_combine(key, mid, value)
+                    self.G.add_edge(key, value)
             print ("mapping finished: {} -> {}".format(code1, code2))
-        
-        if code2 != 'SMILES':
-            if not hasattr(self, "{}_to_{}".format(code2, code1)):
-                if hasattr(self, "{}_to_{}".format(code1, code2)):
-                    exec("self.{0}_to_{1} = self.mapping_reverse_dict(self.{1}_to_{0})".format(code2, code1))
-                else:
-                    shortest_path = nx.shortest_path(self.G, source=code2, target=code1)
-                    key = shortest_path[0]
-                    for i in range(len(shortest_path) - 2):
-                        mid, value = shortest_path[i+1: i+3]
-                        self.map_combine(key, mid, value)
-                        self.G.add_edge(key, value)
-            print ("mapping finished: {} -> {}".format(code2, code1))
                 
-                    
     def map_combine(self, key, mid, value):
         
         if hasattr(self, "{}_to_{}".format(key, mid)):
@@ -162,18 +175,24 @@ class CodeMapping:
             exec("self.{0}_to_{1} = self.mapping_reverse_dict(self.{1}_to_{0})".format(mid, value))
             Dict2 = eval("self.{}_to_{}".format(mid, value))
             
-        exec("self.{}_to_{} = dict()".format(key, value))
+        exec("{}_to_{} = dict()".format(key, value))
+        # do not delete "k"
         for k, m in Dict1.items():
             temp = []
             for m_item in m:
                 if m_item in Dict2:
                     temp += Dict2[m_item]
             if len(temp) > 1:
-                exec("self.{}_to_{}[k] = list(set(temp))".format(key, value))
+                exec("{}_to_{}[k] = list(set(temp))".format(key, value))
+        if hasattr(self, "{}_to_{}".format(key, value)):
+            exec("self.{0}_to_{1}.update({0}_to_{1})".format(key, value))
+        else:
+            exec("self.{0}_to_{1} = {0}_to_{1}".format(key, value))
             
     def add_new_code(self, new_code):
         code_pairs = [(new_code, code) for code in self.Codes if code != new_code]
         for code1, code2 in code_pairs:
             if code2 == code1: continue
             self.add_mapping(code1, code2)
+            self.add_mapping(code2, code1)
         self.Codes.append(new_code)
